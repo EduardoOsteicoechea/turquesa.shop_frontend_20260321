1cbf60f0e4cbbef1003230555ddf179c27afc152
eduar <eduar@EDUARDO>
2026-03-21 08:37:48 -0400

    attempting for first ci/cd

--- src/lib/api/get.ts
+++ src/lib/api/get.ts
@@ -15,5 +15,21 @@ export default async (route: string) => {
 
     console.log(data);
 
+    const response2 = await fetch("https://turquesa.shop/product/images", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (!response.ok) {
+        const errorBody = await response.json().catch(() => ({}));
+        throw new Error(errorBody.message || `API Error: ${response.status}`);
+    }
+
+    const data2 = await response.json();
+
+    console.log(data2);
+
     return data;
 };
